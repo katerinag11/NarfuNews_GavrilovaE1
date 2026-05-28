caf10dfc868f19de9be04aa06f9077bb8b2c8521
katerinag11 <katerinag19112006@gmail.com>
2026-05-28 13:04:12 +0000

    commit

--- Frontend/src/plugins/api/apiConfig.js
+++ Frontend/src/plugins/api/apiConfig.js
@@ -1,14 +1,7 @@
-//Игользуем значение null из .env файла
 const port = import.meta.env.PORT || 3010;
-// Базовый URL вашего API
+
 export const API_BASE_URL = `http://localhost:${port}/api`;
 
-/* Общая функция для выполнения запросов
-* Универсальная функция для выполнения HTTP запросов
-* @param {string} url – относительный путь (например, '/users')
-* @param {object} options – опции fetch (method, body, headers и т.д.)
-* @returns {Promise<any>} – Promise с данными ответа
-*/
 
 export async function fetchData(url, options = {}) {
     try {
